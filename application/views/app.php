7deeb785f1f51a4cbcdaacac4dd8dbaeee1287cf
Kostas Tsalikidis <tsalikidhs@gmail.com>
2019-08-27 08:34:54 +0300

    initialize AngularJS

--- application/views/app.php
+++ application/views/app.php
@@ -1,1 +1,23 @@
-Angular App
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Betlicious - AngularJS</title>
+  <meta name="description" content="For the reason of Web Development skills demonstration">
+  <meta name="author" content="K.Tsalikidis">
+
+  
+
+</head>
+
+<body ng-app="BetliciousApp" ng-controller="AppController as vm">
+  
+  {{vm.foo}}
+
+  <script src="assets/lib/angular.min.js"></script>
+  <script src="assets/js/app.js"></script>
+  <script src="assets/js/app.controller.js"></script>
+</body>
+</html>
---
+++ assets/js/app.controller.js
@@ -0,0 +1,7 @@
+angular
+  .module('BetliciousApp')
+  .controller('AppController', ['$scope', function($scope) {
+    var vm = this;
+
+    vm.foo = 'bar';
+  }]);
